73ddd9e5d6adca6205989029ccdfe3b9a79ce70a
DIVYANSHURATHAUR <90723821+DIVYANSHURATHAUR@users.noreply.github.com>
2023-03-10 01:14:54 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -28,7 +28,6 @@ const PORT = process.env.PORT || 5000;
 
 const DATABASE_URL= process.env.CONNECTION_URL
 
-mongoose.set("strictQuery", true);
 mongoose
   .connect(DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() =>
